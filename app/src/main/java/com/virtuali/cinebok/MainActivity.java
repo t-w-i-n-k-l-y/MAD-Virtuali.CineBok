652a9fdb1f1f49e551124fa519dfe47ab4302a91
t-w-i-n-k-l-y <hkalani00@gmail.com>
2021-09-22 20:48:18 +0530

    Basic files with starting activity, Home and DB connection

---
+++ app/src/main/java/com/virtuali/cinebok/MainActivity.java
@@ -0,0 +1,26 @@
+package com.virtuali.cinebok;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        final Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            public void run() {
+                // TODO: Your application init goes here.
+                Intent mInHome = new Intent(MainActivity.this, HomeActivity.class);
+                startActivity(mInHome);
+                finish();
+            }
+        }, 3000);
+    }
+}
